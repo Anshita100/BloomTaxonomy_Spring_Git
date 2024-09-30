4126834e1d92c5fb5f94f88e869e992b91d00e41
Anshita100 <anshitagupta2277@gmail.com>
2024-09-30 15:04:59 +0530

    created controller and checking index.html

---
+++ src/main/java/com/boss/controller/FileUploadController.java
@@ -0,0 +1,14 @@
+package com.boss.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FileUploadController {
+
+	@GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
